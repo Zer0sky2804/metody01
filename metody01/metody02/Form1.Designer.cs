30443d9032568a241cd0d80e3cf1f35ee8eacef5
Zer0sky <98915550+Zer0sky2804@users.noreply.github.com>
2022-04-28 11:25:18 +0200

    Add files via upload

--- metody01/metody02/Form1.Designer.cs
+++ metody01/metody02/Form1.Designer.cs
@@ -36,6 +36,11 @@
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -99,16 +104,65 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(322, 91);
+            this.listBox1.Location = new System.Drawing.Point(51, 71);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(145, 251);
             this.listBox1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "povodni pole";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(281, 71);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(132, 251);
+            this.listBox2.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(278, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "vysledne pole";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(51, 356);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(51, 392);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "label7";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
@@ -134,6 +188,11 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- metody01/metody02/Form1.cs
+++ metody01/metody02/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using poleUnit;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,76 +18,37 @@ namespace metody02
             InitializeComponent();
         }
 
-        static private int[] Generuj(int n )
-        {
-            Random nh = new Random();
-            int [] x= new int[n];
-            int dol = 1;
-            int hor = 100;
-            for (int i = 0; i < n; i++)
-            {
-                 x[i] = nh.Next(dol, hor);
-                
-            }
-            return x;
-        }
-
-
-        static private void vypis(int[]pole,ListBox lb)
-        {
-            lb.Items.Clear();
-            for (int i = 0; i < pole.Length; i++)
-            {
-                
-                lb.Items.Add(pole[i]);
-            }
-        }
-
-
-        static private int ls(int[]pole,out int suda, out int licha)
-        {
-
-
-            for (int i = 0; i < pole.Length; i++)
-            {
-                if (pole[i] % 2 == 0)
-                {
-                    suda += pole[i];
-                }
-                else
-                {
-                    licha++;
-                }
-            }
-        }
-
-
-
 
         private void button1_Click(object sender, EventArgs e)
         {     int[] pole;
             int n = int.Parse(textBox1.Text);
             if (textBox2.Text == "" && textBox3.Text == "")
             {
-                pole = Generuj(n);
+                pole = Pole.Generuj(n);
             }
             else
             {
             int a=int.Parse(textBox2.Text);
             int b=int.Parse(textBox3.Text);
-
+                pole = Pole.Gen(n, a, b);   
             }
-            int sude = 0;
-            int liche = 0;
-            int max = 0;
-            int pmax=0;
-       
-            
-                if (x[i] > max)
-                {
-                    max = x[i];
-                    pmax = i;
-                }
+
+            Pole.vypis(pole, listBox1);
+            int sude;
+            int liche;
+            bool ros = Pole.rost(pole);
+            Pole.lichasuda(pole, out sude, out liche);
+            Pole.vymena(pole);
+            Pole.vypis(pole, listBox2);
+            label6.Text = "Soucet cudych je " + sude + " pocet lichych je " + liche;
+            if (ros)
+            {
+                label7.Text = "Funkce je rostouci";
+            }
+            else
+            {
+                label7.Text = "Funkce neni rostouci";
+            }
 
         }
     }
