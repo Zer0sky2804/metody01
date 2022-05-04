891433eddbe725373bdd18519e5e52e730590a20
Zer0sky <98915550+Zer0sky2804@users.noreply.github.com>
2022-05-04 09:33:16 +0200

    Add files via upload

---
+++ metody01/knihovna03/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace knihovna03
+{
+    public class Class1
+    {
+        static public int obsah(int a, int b, out double u)
+        {
+            int ob = a * b;
+            u = Math.Sqrt((a * a) + (b * b));
+            return ob;
+        }
+
+        static public int k(int a, int b, int c, out double u)
+        {
+            int o = 2 * (a * b + b * c + c * a);
+            u = Math.Sqrt((a * a) + (b * b) + (c * c));
+            return o;
+        }
+    }
+}
--- metody01/matody03/Form1.cs
+++ metody01/matody03/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using knihovna03;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,47 @@ namespace matody03
         public Form1()
         {
             InitializeComponent();
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            double u;
+            int o = Class1.obsah(a, b, out u);
+            if (a == b)
+            {
+                label4.Text = "Obsah ctverce je " + o + " a uholpricke je " + u;
+            }
+            else
+            {
+                label4.Text = "Obsah obdelniku je " + o + " a uholpricke je " + u;
+            }
+        }
+        
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            int c = int.Parse(textBox3.Text);
+            double u;
+            int o = Class1.k(a, b, c, out u);
+            if (a == b && b == c)
+            {
+                label5.Text = "osah krychle je " + o + " a uhlopricka je " + u;
+            }
+            else
+            {
+                label5.Text = "osah krychle je " + o + " a uhlopricka je " + u;
+            }
         }
     }
 }
