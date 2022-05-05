02cac426efcb3ed5c0643f3868b9192e67052886
Zer0sky <98915550+Zer0sky2804@users.noreply.github.com>
2022-05-05 11:35:14 +0200

    Add files via upload

---
+++ MatematikaUtil/Matemaitka.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MatematikaUtil
+{
+    public class Matemaitka
+    {
+        static public int Mocnina(int cislo,int x)
+        {
+            int moc = 1;
+            for (int p = 0; p < x; p++)
+            {
+                moc *= cislo;
+            }
+            return moc;
+        }
+    }
+}
